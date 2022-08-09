1692a4a62b30c5221f458690a89d6d22675d8b8f
Apex4598 <login2ajit@gmail.com>
2022-08-09 08:42:15 +0530

    Logicl Program

---
+++ src/com/bridgelabz/Logical.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Logical {
+    public void couponNumbers(){
+
+    }
+
+    public static void main(String[] args) {
+        Logical logical = new Logical();
+        logical.couponNumbers();
+    }
+}
